df42fef52e85011021b2ef2514c01d2e17799e15
xbzgcz <55831586+xbzgcz@users.noreply.github.com>
2020-01-10 15:52:37 +0800

    Update telephonemanage.sql

--- 通讯录/telephonemanage.sql
+++ 通讯录/telephonemanage.sql
@@ -1,14 +1,8 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : MySQL57
-Source Server Version : 50725
 Source Host           : localhost:3306
 Source Database       : telephonemanage
-
-Target Server Type    : MYSQL
-Target Server Version : 50725
-File Encoding         : 65001
 
 Date: 2020-01-10 14:13:12
 */
